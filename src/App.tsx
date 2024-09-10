f6f020b4e6ee6d629e5d0187f8b9cacb2b6c27c6
Rody Huancas <rodyhuancas.04@gmail.com>
2024-09-10 08:04:51 -0500

    Feat: Textare

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
-import { Badge } from "./components/Badge";
-import { Button } from "./components/Button";
-import { Checkbox } from "./components/Checkbox";
+import { Badge } from './components/Badge'
+import { Button } from './components/Button'
+import { Checkbox } from './components/Checkbox'
+import { Textarea } from './components/Textarea'
 
 const App = () => {
   return (
-    <div className="flex items-center justify-center gap-5 h-screen">
-      <Button size="large" variant="danger">
-        Click Me
-      </Button>
+    <div className='flex flex-col items-center justify-center gap-5 h-screen'>
+      <div className="flex gap-5">
+        <Button size="large" variant="danger">
+          Click Me
+        </Button>
 
-      <Badge label="Active" type="success" />
+        <Badge label="Active" type="success" />
 
-      <Checkbox label="Check Me" />
+        <Checkbox label="Check Me" />
+      </div>
+      
+      <div className='flex gap-5'>
+        <Textarea label="Message" />
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
