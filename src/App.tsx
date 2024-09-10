9bcb55ee6a884d378065a2aae485561b7e47d774
Rody Huancas <rodyhuancas.04@gmail.com>
2024-09-09 19:51:50 -0500

    Feat: Bagde

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { Badge } from "./components/Badge";
 import { Button } from "./components/Button";
 
 const App = () => {
   return (
-    <div className="flex items-center justify-center gap-10 h-screen">
+    <div className="flex items-center justify-center gap-5 h-screen">
       <Button size="large" variant="danger">
         Click Me
       </Button>
+
+      <Badge label="Active" type="success" />
     </div>
   );
 };
