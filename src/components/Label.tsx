dbe504c9ffc559f46e32ccb4b5f19eb5c344d8f5
Rody Huancas <rodyhuancas.04@gmail.com>
2024-09-14 22:42:20 -0500

    Feat: label component

---
+++ src/components/Label.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {
+  text: string;
+  htmlFor?: string;
+  className?: string;
+}
+
+const Label: React.FC<LabelProps> = ({ htmlFor, text, className, ...props }) => {
+  return (
+    <label
+      htmlFor={htmlFor}
+      className={`block text-sm font-medium text-gray-700 ${className}`}
+      {...props}
+    >
+      {text}
+    </label>
+  );
+};
+
+export default Label;
